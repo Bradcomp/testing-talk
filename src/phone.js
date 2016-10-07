70c361600288409c45466cd1ec683f0c479a2895
Bradcomp <notpmoc84@hotmail.com>
2016-10-06 22:41:39 -0700

    First passing test!

--- src/phone.js
+++ src/phone.js
@@ -1,8 +1,14 @@
 'use strict';
 const {Success, Failure} = require('data.validation');
 
+const isString = (s) => (typeof s === 'string');
+
 const phone = (phoneNumber) => {
-  if (phoneNumber.replace(/\D+/, '').length !== 9) return Failure(['invalid phone number']);
+  if (!isString(phoneNumber))
+    return Failure(['invalid phone number']);
+    
+  if (phoneNumber.replace(/\D+/, '').length !== 9)
+    return Failure(['invalid phone number']);
 
   return Success(phoneNumber.replace(/\D+/, ''));
 };
