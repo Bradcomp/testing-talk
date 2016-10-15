aef6f46bccdb0be8094293f231b1712a2634a0f7
Bradcomp <notpmoc84@hotmail.com>
2016-10-15 16:18:10 -0700

    second passing test

--- test/phone.js
+++ test/phone.js
@@ -6,15 +6,19 @@ const { Success, Failure } = require('data.validation');
 const phone = require('../src/phone');
 
 describe('Phone number validation', () => {
+  const failure = Failure(['invalid phone number']);
 
   it('should require the phone number to be a string', () => {
-    const failure = Failure(['invalid phone number']);
-
     expect(phone(undefined)).to.eql(failure);
     expect(phone(123456789)).to.eql(failure);
   });
 
-  it('should require the phone number to be 10 digits');
+  it('should require the phone number to be 10 digits', () => {
+    expect(phone('32345')).to.eql(failure);
+    expect(phone('423456789')).to.eql(failure);
+    expect(phone('52345678901')).to.eql(failure);
+    expect(phone('9165554444')).to.eql(Success('9165554444'));
+  });
 
   it('should strip non-digit characters from the phone string');
 });
